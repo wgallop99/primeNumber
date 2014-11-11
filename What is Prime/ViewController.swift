f1e4b668db336ec69fb988bd5bb6a3d2c4720f6a
Will Gallop <wgallop99@gmail.com>
2014-11-10 20:30:20 -0500

    still error filled

--- What is Prime/ViewController.swift
+++ What is Prime/ViewController.swift
@@ -9,6 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var Integer: UITextField!
+    
+    @IBOutlet weak var message: UILabel!
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        var isPrime = true
+        
+        var Integer = number.text.toInt()
+        
+        if((number) != nil) {
+            
+            if(Integer<1){
+                message.text = "Please enter a positive number!"
+            } else {
+                
+                if(Integer! ==1){
+                    message.text = "1 is not prime!"
+                } else {
+                   
+                    for var i = 2; i<Integer; i++{
+                        
+                        if(Integer! % i == 0){
+                            //Number is NOT prime
+                            
+                            isPrime = false;
+                            
+                            
+                        }
+                    }
+                    
+                    if(isPrime = true) {
+                        message.text = "That number is prime!"
+                        
+                    } else {
+                        message.text = "NOT PRIME!"
+                    
+                    }
+                }
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
