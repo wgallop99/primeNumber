77e788b9818a82a960f209fc4f5b999bf947a6a4
Will Gallop <wgallop99@gmail.com>
2014-11-10 19:41:18 -0500

    Initial Commit

---
+++ What is Prime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What is Prime
+//
+//  Created by Will Gallop on 11/10/14.
+//  Copyright (c) 2014 Will Gallop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ What is PrimeTests/What_is_PrimeTests.swift
@@ -0,0 +1,36 @@
+//
+//  What_is_PrimeTests.swift
+//  What is PrimeTests
+//
+//  Created by Will Gallop on 11/10/14.
+//  Copyright (c) 2014 Will Gallop. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class What_is_PrimeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
